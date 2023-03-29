0197a88f569f436698b37e27d4d42e93dee2dc14
iCanOzcan <123066429+iCanOzcan@users.noreply.github.com>
2023-03-29 09:02:22 +0300

    Update VucutKitleIndex.java

--- src/VucutKitleIndex.java
+++ src/VucutKitleIndex.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class VucutKitleIndex {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         double kilo,boy,bmi;
         System.out.print("Boyunuzu metre cinsinden girin:");
